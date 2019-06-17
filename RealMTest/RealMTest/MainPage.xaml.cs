88e6fcf5ea8bdb4e28c57514d3edbd72f96134ca
v-zhumin <v-zhumin@DESKTOP-KNMM69A>
2019-06-17 10:40:30 +0800

    Add project files.

---
+++ RealMTest/RealMTest/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RealMTest
+{
+    public class Product : RealmObject
+    {
+        public string Name { get; set; }
+    }
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public static string LocalPath = "";
+        public MainPage()
+        {
+            InitializeComponent();
+            var realm = Realm.GetInstance(Path.Combine(LocalPath, "RealmSample.realm"));
+            realm.Write(() =>
+            {
+                realm.Add(new Product()
+                {
+                    Name = "Test"
+                });
+            });
+            BindingContext = realm.All<Product>().FirstOrDefault();  
+        }
+    }
+}
